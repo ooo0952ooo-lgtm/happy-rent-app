997aae672573f73757c9835624a0b562268bda11
ooo0952ooo-lgtm <ooo0952ooo@gmail.com>
2026-05-05 10:37:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ prompt_text = f"""
 """
 
 # 4. 앱 화면 구성 및 실행 로직 (기존과 동일)
-st.set_page_config(page_title="제주 서류 검수기", layout="centered")
+st.set_page_config(page_title="해피 외국인 서류검수", layout="centered")
 st.title("🔍외국인 서류 검수 시스템")
 st.write(f"📅 오늘 날짜 {today_date}") # 화면에도 오늘 날짜를 표시해줍니다.
 
